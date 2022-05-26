4d42b9921843e599c50361237bc01f12da939447
Brian Hulette <hulettbh@gmail.com>
2022-05-26 06:59:54 -0700

    move to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://theneuralbit.com'
+SITEURL = 'https://theneuralbit.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
